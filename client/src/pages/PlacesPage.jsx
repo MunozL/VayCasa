8a169fd7cc01f4e6396eca1deec2cc4a98bc528a
Lizette <Lizettemu1@gmail.com>
2023-10-03 01:48:48 -0500

    Added PUT functionality/Endpoint to update listing

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -47,9 +47,13 @@ export default function PlacesPage() {
               to={"/account/places/" + place._id}
               className="cursor-pointer flex gap-4 bg-gray-200 p-2 rounded-2xl"
             >
-              <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+              <div className=" flex w-32 h-32 bg-gray-300 grow shrink-0">
                 {place.photos.length > 0 && (
-                  <img src={place.photos[0]} alt="" />
+                  <img
+                    className="object-cover"
+                    src={"http://localhost:4000/" + place.photos[0]}
+                    alt=""
+                  />
                 )}
               </div>
               <div className="grow-0 shrink">
