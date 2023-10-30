74c58e9b1928a9ebaea644a87f64a1fb69a30d9d
Lizette <Lizettemu1@gmail.com>
2023-10-30 01:50:17 -0500

    added url for vercel

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://vaycasa.vercel.app"], // Allow both localhost and your production origin
   })
 );
 
